4c72a18a963f4ac42e0dfff4a9f6039c33314179
David Marek <34945560+davmarek@users.noreply.github.com>
2021-10-20 20:19:49 +0200

    Build and prettierrc

--- src/components/Meal.vue
+++ src/components/Meal.vue
@@ -1,64 +1,64 @@
 <template>
-    <div class="meal" :class="{ disabled: !this.available }">
-        <div class="name">{{ this.name }}</div>
-        <div class="price">
-            <div v-if="price > 0" class="price-chip">{{ this.price }} Kč</div>
-        </div>
+  <div class="meal" :class="{ disabled: !this.available }">
+    <div class="name">{{ this.name }}</div>
+    <div class="price">
+      <div v-if="price > 0" class="price-chip">{{ this.price }} Kč</div>
     </div>
+  </div>
 </template>
 
 <script>
 export default {
-    props: {
-        name: {
-            type: String,
-            required: true,
-        },
-        price: {
-            type: Number,
-            required: false,
-            default: 0,
-        },
-        available: {
-            type: Boolean,
-            required: false,
-        },
+  props: {
+    name: {
+      type: String,
+      required: true,
     },
+    price: {
+      type: Number,
+      required: false,
+      default: 0,
+    },
+    available: {
+      type: Boolean,
+      required: false,
+    },
+  },
 };
 </script>
 
 <style>
 .meal {
-    background-color: white;
-    display: grid;
-    grid-template-columns: 1fr auto;
+  background-color: white;
+  display: grid;
+  grid-template-columns: 1fr auto;
 
-    padding: 8px 16px;
+  padding: 8px 16px;
 }
 .meal:not(.meal:last-child) {
-    border-bottom: 1px solid #c8c8c8;
+  border-bottom: 1px solid #c8c8c8;
 }
 
 .name {
-    padding-right: 16px;
-    display: flex;
-    align-items: center;
+  padding-right: 16px;
+  display: flex;
+  align-items: center;
 }
 
 .price-chip {
-    padding: 4px 10px;
-    color: white;
-    background-color: #007aff;
-    border-radius: 50vh;
+  padding: 4px 10px;
+  color: white;
+  background-color: #007aff;
+  border-radius: 50vh;
 
-    font-weight: 500;
+  font-weight: 500;
 }
 
 .disabled {
-    color: #ccc;
+  color: #ccc;
 }
 
 .disabled .price-chip {
-    background-color: #eee;
+  background-color: #eee;
 }
 </style>
